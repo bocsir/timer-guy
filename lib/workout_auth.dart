6eb34e9e0ed4abb3f764d48c5f8612498804b484
bocsir <jacobwharris13@gmail.com>
2025-10-04 00:56:12 -0400

    validation

--- lib/workout_auth.dart
+++ lib/workout_auth.dart
@@ -1,5 +1,6 @@
 // workout_auth.dart
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:forui/forui.dart';
 import 'package:proj/theme/theme.dart';
 
@@ -20,71 +21,176 @@ class _WorkoutAuthState extends State<WorkoutAuth> {
 
   final _formKey = GlobalKey<FormState>();
 
+  bool _validateForm() {
+    if (!_formKey.currentState!.validate()) {
+      return false;
+    }
+
+    final repCount = _controllers['repCount'].text.trim();
+    final setCount = _controllers['setCount'].text.trim();
+
+    if (repCount.isEmpty || setCount.isEmpty) {
+      return false;
+    }
+
+    final repDuration = _controllers['repDuration'].selectedIndexes;
+    final restDuration = _controllers['restDuration'].selectedIndexes;
+
+    if (!_isTimeGreaterThanZero(repDuration) ||
+        !_isTimeGreaterThanZero(restDuration)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool _isTimeGreaterThanZero(List<int> timeIndexes) {
+    final hours = timeIndexes[0] % 12;
+    final minutes = timeIndexes[1] % 60;
+    final seconds = timeIndexes[2] % 60;
+    return hours > 0 || minutes > 0 || seconds > 0;
+  }
+
+  void _onDonePressed() {
+    if (_validateForm()) {
+      // do stuff
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 16,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return Column(
+      spacing: 32,
+      children: [
+        Text(
+          'Create Workout',
+          style: FTheme.of(context).typography.xl.copyWith(
+            fontWeight: FontWeight.w700,
+            fontFamily: 'IBMPlexMono',
+            height: 0,
+          ),
+        ),
+        Form(
+          key: _formKey,
+          child: Column(
+            spacing: 32,
             children: [
-              Text('On'),
-              ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: 150, maxWidth: 150),
-                child: FPicker(
-                  controller: _controllers['repDuration'],
-                  children: [
-                    FPickerWheel.builder(
-                      builder: (context, index) =>
-                          Text((index % 12).toString().padLeft(2, '0')),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: FTextFormField(
+                      label: Text(
+                        'Reps',
+                        style: FTheme.of(context).typography.lgSemibold,
+                      ),
+                      controller: _controllers['repCount'],
+                      keyboardType: TextInputType.numberWithOptions(),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Required';
+                        }
+                        final intValue = int.tryParse(value);
+                        if (intValue == 0) {
+                          return '0 reps?';
+                        }
+                        return null;
+                      },
                     ),
-                    const Text(':'),
-                    FPickerWheel.builder(
-                      builder: (context, index) =>
-                          Text((index % 60).toString().padLeft(2, '0')),
+                  ),
+                  SizedBox(width: 64),
+                  Expanded(
+                    child: FTextFormField(
+                      label: Text(
+                        'Sets',
+                        style: FTheme.of(context).typography.lgSemibold,
+                      ),
+                      controller: _controllers['setCount'],
+                      keyboardType: TextInputType.numberWithOptions(),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Required';
+                        }
+                        final intValue = int.tryParse(value);
+                        if (intValue == 0) {
+                          return '0 sets?';
+                        }
+                        return null;
+                      },
                     ),
-                    const Text(':'),
-                    FPickerWheel.builder(
-                      builder: (context, index) =>
-                          Text((index % 60).toString().padLeft(2, '0')),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+              Column(
+                children: [
+                  Text(
+                    'Time On',
+                    style: FTheme.of(context).typography.lgSemibold,
+                  ),
+                  Text('hh:mm:ss', style: FTheme.of(context).typography.smGrey),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxHeight: 150),
+                    child: FPicker(
+                      controller: _controllers['repDuration'],
+                      children: [
+                        FPickerWheel.builder(
+                          builder: (context, index) =>
+                              Text((index % 12).toString().padLeft(2, '0')),
+                        ),
+                        const Text(':'),
+                        FPickerWheel.builder(
+                          builder: (context, index) =>
+                              Text((index % 60).toString().padLeft(2, '0')),
+                        ),
+                        const Text(':'),
+                        FPickerWheel.builder(
+                          builder: (context, index) =>
+                              Text((index % 60).toString().padLeft(2, '0')),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Time Off',
+                    style: FTheme.of(context).typography.lgSemibold,
+                  ),
+                  Text('hh:mm:ss', style: FTheme.of(context).typography.smGrey),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxHeight: 150),
+                    child: FPicker(
+                      controller: _controllers['restDuration'],
+                      children: [
+                        FPickerWheel.builder(
+                          builder: (context, index) =>
+                              Text((index % 12).toString().padLeft(2, '0')),
+                        ),
+                        const Text(':'),
+                        FPickerWheel.builder(
+                          builder: (context, index) =>
+                              Text((index % 60).toString().padLeft(2, '0')),
+                        ),
+                        const Text(':'),
+                        FPickerWheel.builder(
+                          builder: (context, index) =>
+                              Text((index % 60).toString().padLeft(2, '0')),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              // Done button
+              FButton(onPress: _onDonePressed, child: const Text('Done')),
             ],
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('Off'),
-              ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: 150, maxWidth: 150),
-                child: FPicker(
-                  controller: _controllers['restDuration'],
-                  children: [
-                    FPickerWheel.builder(
-                      builder: (context, index) =>
-                          Text((index % 12).toString().padLeft(2, '0')),
-                    ),
-                    const Text(':'),
-                    FPickerWheel.builder(
-                      builder: (context, index) =>
-                          Text((index % 60).toString().padLeft(2, '0')),
-                    ),
-                    const Text(':'),
-                    FPickerWheel.builder(
-                      builder: (context, index) =>
-                          Text((index % 60).toString().padLeft(2, '0')),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
