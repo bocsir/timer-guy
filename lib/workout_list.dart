9e81d01c769f6016a359be3e7f443e85a2ed768d
bocsir <jacobwharris13@gmail.com>
2025-10-05 22:34:42 -0400

    unused import

--- lib/workout_list.dart
+++ lib/workout_list.dart
@@ -1,7 +1,6 @@
 // workout_list.dart
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
-import 'package:proj/header.dart';
 
 class WorkoutList extends StatelessWidget {
   const WorkoutList({super.key});
