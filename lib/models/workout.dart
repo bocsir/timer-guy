4a0f5f96e0f75dc3df627340da22af22d7b394f5
bocsir <jacobwharris13@gmail.com>
2025-10-05 22:24:24 -0400

    workout model

---
+++ lib/models/workout.dart
@@ -0,0 +1,32 @@
+// models/workout.dart
+class Workout {
+  final String name;
+  final int reps;
+  final int sets;
+  final String timeOn; //use better type
+  final String timeOff; //use better type
+
+  Workout({
+    required this.name,
+    required this.reps,
+    required this.sets,
+    required this.timeOn,
+    required this.timeOff,
+  });
+
+  Workout copyWith({
+    String? name,
+    int? reps,
+    int? sets,
+    String? timeOn,
+    String? timeOff,
+  }) {
+    return Workout(
+      name: name ?? this.name,
+      reps: reps ?? this.reps,
+      sets: sets ?? this.sets,
+      timeOn: timeOn ?? this.timeOn,
+      timeOff: timeOff ?? this.timeOff,
+    );
+  }
+}
