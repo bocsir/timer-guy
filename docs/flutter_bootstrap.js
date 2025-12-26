9617970bcc3172eee84adb1dc963ae07f7672689
bocsir <jacobwharris13@gmail.com>
2025-12-25 23:45:25 -0500

    deploy to pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "955093696"
+    serviceWorkerVersion: "3440012950"
   }
 });
